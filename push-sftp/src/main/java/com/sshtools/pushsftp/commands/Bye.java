ffd519dc57812164a3bd0d32cd85b7484e315f0b
Lee Painter <lee@ludup.com>
2023-03-26 21:19:46 +0100

    Add quit and exit aliases to Bye command

--- push-sftp/src/main/java/com/sshtools/pushsftp/commands/Bye.java
+++ push-sftp/src/main/java/com/sshtools/pushsftp/commands/Bye.java
@@ -2,7 +2,7 @@ package com.sshtools.pushsftp.commands;
 
 import picocli.CommandLine.Command;
 
-@Command(name = "bye", usageHelpAutoWidth = true, mixinStandardHelpOptions = false, description = "Quit interactive command")
+@Command(name = "bye", usageHelpAutoWidth = true, aliases = { "quit", "exit" }, mixinStandardHelpOptions = false, description = "Quit interactive command")
 public class Bye extends SftpCommand {
 
 	@Override
