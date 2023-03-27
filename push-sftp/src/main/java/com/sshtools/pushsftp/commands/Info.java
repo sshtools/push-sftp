c6b7ed94662610577457edf07d5059168e2d5e3d
Lee Painter <lee@ludup.com>
2023-03-27 01:08:24 +0100

    Add optional SFTP statistics to the info command, if transfers have been performed then various runtime statistics are shown which may be helpful to analysing test results in future

--- push-sftp/src/main/java/com/sshtools/pushsftp/commands/Info.java
+++ push-sftp/src/main/java/com/sshtools/pushsftp/commands/Info.java
@@ -1,5 +1,7 @@
 package com.sshtools.pushsftp.commands;
 
+import java.util.Objects;
+
 import com.sshtools.client.SshClient;
 import com.sshtools.common.publickey.SshKeyUtils;
 
@@ -11,10 +13,15 @@ public class Info extends SftpCommand {
 	@Override
 	protected Integer onCall() throws Exception {
 		SshClient ssh = getSshClient();
+		
+		getTerminal().messageln("-HOST-----------------");
 		getTerminal().messageln("Hostname             : {0}", ssh.getHost());
 		getTerminal().messageln("Port                 : {0}", ssh.getPort());
 		getTerminal().messageln("Username             : {0}", ssh.getConnection().getUsername());
 		getTerminal().messageln("Remote identification: {0}", ssh.getRemoteIdentification());
+		getTerminal().messageln("");
+		
+		getTerminal().messageln("-KEX------------------");
 		getTerminal().messageln("Host key type        : {0}", ssh.getConnection().getHostKeyAlgorithm());
 		getTerminal().messageln("Host key fingerprint : {0}", SshKeyUtils.getFingerprint(ssh.getHostKey()));
 		getTerminal().messageln("Key exchange         : {0}", SshKeyUtils.getFingerprint(ssh.getConnection().getKeyExchangeInUse()));
@@ -24,10 +31,34 @@ public class Info extends SftpCommand {
 		getTerminal().messageln("Mac (s->c)           : {0}", ssh.getConnection().getMacInUseSC());
 		getTerminal().messageln("Compression (c->s)   : {0}", ssh.getConnection().getCompressionInUseCS());
 		getTerminal().messageln("Compression (s->c)   : {0}", ssh.getConnection().getCompressionInUseSC());
+		getTerminal().messageln("");
+		getTerminal().messageln("-SFTP-----------------");	
 		
+		if(showSFTPStatistic(      "Write blocksize      : {0}ms", "maverick.write.optimizedBlock")) {
+			showSFTPStatistic(      "Write roundtrip      : {0}ms", "maverick.write.blockRoundtrip");
+			showSFTPStatistic(      "Write async requests : {0}", "maverick.write.asyncRequests"); 
+		} else {
+			getTerminal().messageln("Perform a put or push operation to generate SFTP write operation statistics");
+		}
+		if(showSFTPStatistic(      "Read blocksize       : {0}ms", "maverick.read.optimizedBlock")) {
+			showSFTPStatistic(      "Read roundtrip       : {0}ms", "maverick.read.blockRoundtrip");
+			showSFTPStatistic(      "Read async requests  : {0}", "maverick.read.asyncRequests");
+		} else {
+			getTerminal().messageln("Perform a get or pull operation to generate SFTP read operation statistics");
+		}
 		
-		getTerminal().messageln("Remote identification: {0}", ssh.getRemoteIdentification());
-		
+		getTerminal().messageln("");
+	
 		return 0;
+	}
+
+	private boolean showSFTPStatistic(String message, String value) {
+		String v = System.getProperty(value);
+		if(Objects.isNull(v)) {
+			return false;
+		}
+		
+		getTerminal().messageln(message, v);
+		return true;
 	}
 }
